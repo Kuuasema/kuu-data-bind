bf7c07c9032ac61b05e74727441aa3daed13848e
Jaakko Holopainen <jaakko.holopainen@kuuasema.com>
2025-01-28 18:13:58 +0200

    Custom viewbindings class editor

--- Editor/ViewBindingsEditor.cs
+++ Editor/ViewBindingsEditor.cs
@@ -4,195 +4,81 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using Kuuasema.Utils;
+using System.Linq;
 
 namespace Kuuasema.DataBinding.Editor {
-    [UnityEditor.CustomEditor(typeof(ViewBindings), true)]
-    public class ViewBindingsEditor : UnityEditor.Editor {
 
-    //     public static bool DrawProperty { get; private set;}
-    //     private static GUIContent guiContentBoundViews = new GUIContent("Bound Views");
-    //     public override void OnInspectorGUI() {
+    [CustomPropertyDrawer(typeof(ViewBindings))]
+    public class ViewBindingsDrawer : PropertyDrawer {
 
-    //         // this.DrawDefaultInspector();
+        private static GUIStyle styleButtonCreate;
+        private static GUIStyle styleButtonDelete;
+        private static int indent = 0;
 
-    //         ViewModel viewModel = (ViewModel) target;
-    //         DataModel dataModel = viewModel.GetDataModel();
+        private static void InitStyles() {
+            if (styleButtonCreate == null) {
+                styleButtonCreate = new GUIStyle(GUI.skin.button);
+                styleButtonCreate.normal.textColor = Color.green;
+            }
+            if (styleButtonDelete == null) {
+                styleButtonDelete = new GUIStyle(GUI.skin.button);
+                styleButtonDelete.normal.textColor = Color.red;
+            }
+        }
 
-    //         serializedObject.Update();
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+            InitStyles();
+            EditorGUI.BeginProperty(position, label, property);
 
-    //         bool isPlaying = Application.isPlaying;
+            if (indent == 0) {
+                Rect boxRect = position;
+                boxRect.x -= 1;
+                boxRect.y -= 1;
+                boxRect.width += 2;
+                boxRect.height += 2;
+                GUI.Box(boxRect, "", EditorStyles.helpBox);
+            }
+
+            indent++;
+
+            FieldInfo field = property.GetTargetField();
+            if (field == null || field.GetCustomAttributes(typeof(SerializeReference), false).FirstOrDefault() == null) {
+                EditorGUI.PropertyField(position, property, label, true);
+            } else {
             
-		
-	// 		SerializedProperty prop = serializedObject.GetIterator();
+                Rect labelRect = position;
+                labelRect.width = EditorGUIUtility.labelWidth;
+                labelRect.height = 16;
 
-    //         EditorGUILayout.BeginVertical();
-	// 		if (prop.NextVisible(true)) {
-	// 			do {
-    //                 bool showData = false;
-    //                 if (isPlaying && prop.propertyType == SerializedPropertyType.Generic && prop.isArray) {
-    //                     if (prop.arrayElementType.StartsWith("PPtr<$ViewModel")) {
-    //                         showData = true;
-    //                     }
-    //                 }
-                    
-    //                 if (showData) {
-                        
-                        
-
-    //                     EditorGUILayout.BeginHorizontal();
-
-    //                     string label = prop.displayName;
-    //                     string key = prop.name;
-
-    //                     prop.isExpanded = GUILayout.Toggle(prop.isExpanded, label, EditorStyles.foldout, GUILayout.Width(EditorGUIUtility.labelWidth));
-                        
-    //                     EditorGUILayout.LabelField("[D]", EditorStyles.boldLabel, GUILayout.MaxWidth(20));
-
-    //                     DataModel _dataModel = dataModel?.Find(key);
-    //                     if (_dataModel == null) {
-    //                         GUILayout.Label("<NO DATA BOUND>");
-    //                     } else {
-    //                         this.DrawDataField(_dataModel);
-    //                     }
-
-    //                     EditorGUILayout.EndHorizontal();
-
-                       
-
-    //                     if (prop.isExpanded) {
-    //                         EditorGUILayout.BeginVertical("Box");
-    //                         DrawProperty = false;
-    //                         EditorGUI.indentLevel++;
-
-
-    //                         if (_dataModel != null) {
-    //                             EditorGUI.indentLevel++;
-    //                             EditorGUILayout.BeginVertical();
-    //                             foreach (KeyValuePair<string,DataModel> keyVal in _dataModel.BindingMap) {
-    //                                 EditorGUILayout.BeginHorizontal();
-    //                                 GUILayout.Label("", GUILayout.Width(EditorGUIUtility.labelWidth / 2));
-    //                                 GUILayout.Label(keyVal.Key, GUILayout.Width(EditorGUIUtility.labelWidth / 2));
-
-    //                                 // GUILayout.Label("<ADD DATA FIELD>");
-    //                                 this.DrawDataField(keyVal.Value);
-
-    //                                 EditorGUILayout.EndHorizontal();
-    //                             }
-                                
-    //                             EditorGUILayout.EndVertical();
-    //                             EditorGUI.indentLevel--;
-    //                         }
-
-                            
-    //                         EditorGUILayout.PropertyField(prop, guiContentBoundViews, true);
-    //                         EditorGUI.indentLevel--;
-    //                         DrawProperty = true;
-    //                         EditorGUILayout.EndVertical();
-    //                     }
-
-                        
-
-    //                 } else {
-	// 					EditorGUILayout.PropertyField(prop, true);
-	// 				}
-	// 			}
-	// 			while (prop.NextVisible(false));
-	// 		}
-    //         EditorGUILayout.EndVertical();
-		
-	// 		serializedObject.ApplyModifiedProperties();
-    //     }     
-
-    //     protected virtual void DrawDataField(DataModel baseModel) {
-
-    //         switch (baseModel.DataType.Name) {
-    //             case "Boolean": {
-    //                 DataModel<bool> dataModel = baseModel as DataModel<bool>;
-    //                 bool newValue = EditorGUILayout.Toggle(dataModel.Value);
-    //                 if (!dataModel.IsEqual(newValue)) {
-    //                     Debug.Log($"{dataModel.IsEquatable} :: {newValue} != {dataModel.Value}");
-    //                     dataModel.TryChangeValue(newValue);
-    //                 }
-    //             } return;
-    //             case "Single": {
-    //                 DataModel<float> dataModel = baseModel as DataModel<float>;
-    //                 float newValue = EditorGUILayout.FloatField(dataModel.Value);
-    //                 if (!dataModel.IsEqual(newValue)) {
-    //                     dataModel.TryChangeValue(newValue);
-    //                 }
-    //             } return;
-    //             case "Int32": {
-    //                 DataModel<int> dataModel = baseModel as DataModel<int>;
-    //                 int newValue = EditorGUILayout.IntField(dataModel.Value);
-    //                 if (!dataModel.IsEqual(newValue)) {
-    //                     dataModel.TryChangeValue(newValue);
-    //                 }
-    //             } return;
-    //             case "Vector3": {
-    //                 DataModel<Vector3> dataModel = baseModel as DataModel<Vector3>;
-    //                 // Vector3 newValue = EditorGUILayout.Vector3Field((GUIContent)null, dataModel.Value);
-    //                 Vector3 newValue = EditorGUILayout.Vector3Field(GUIContent.none, dataModel.Value);
-    //                 if (!dataModel.IsEqual(newValue)) {
-    //                     dataModel.TryChangeValue(newValue);
-    //                 }
-    //             } return;
-    //             default: {
-    //                 if (typeof(Enum).IsAssignableFrom(baseModel.DataType)) {
-    //                     Enum oldValue = (Enum)baseModel.GetValue();
-    //                     Enum newValue = EditorGUILayout.EnumPopup((GUIContent)null, oldValue);
-    //                     if (oldValue != newValue) {
-    //                         baseModel.SetValue(newValue);
-    //                     }
-    //                     return;
-    //                 }
-    //                 GUILayout.Label($"{baseModel.DataType.Name}");
-    //             } break;
-    //         }
-
-    //         // T newValue = this.DrawField(position, dataModel.Value);
-    //         // if (!dataModel.IsEqual(newValue)) {
-    //         //     dataModel.TryChangeValue(newValue);
-    //         // }
-    //     }   
-    // }
-
-
-    // public class ViewModelDrawer<T> : PropertyDrawer {
-
-    //     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
-    //         EditorGUI.BeginProperty(position, label, property);
-
-    //         ViewModel<T> viewModel = property.objectReferenceValue as ViewModel<T>;
-    //         if (ViewModelEditor.DrawProperty && Application.isPlaying && viewModel != null) {
+                Rect buttonRect = position;
+                buttonRect.x = position.x + position.width - 20;
+                buttonRect.width = 20;
+                buttonRect.height = 16;
                 
-    //             position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
-    //             DataModel<T> dataModel = viewModel.DataModel;
-    //             if (dataModel == null) {
-    //                 GUI.Label(position, "<NULL>");
-    //             } else {
-    //                 this.DrawDataField(position, viewModel, dataModel);
-    //             }
-    //         } else {
-    //             EditorGUI.PropertyField(position, property, label, true);
-    //         }
+                if (property.managedReferenceValue == null) {
+                    labelRect.x += indent * 11;
+                    GUI.Label(labelRect, label);
+                    if (GUI.Button(buttonRect, "+", styleButtonCreate)) {
+                        property.managedReferenceValue = Activator.CreateInstance(field.FieldType);
+                    }
+                } else {
+                    if (GUI.Button(buttonRect, "X", styleButtonDelete)) {
+                        if (EditorUtility.DisplayDialog("Delete Binding", $"Are you sure you want to delete the binding '{field.Name}'?'", "Yes", "No")) {
+                            property.managedReferenceValue = null;
+                        }
+                    }
+                    EditorGUI.PropertyField(position, property, label, true);
+                }
+            }
 
-    //         EditorGUI.EndProperty();
-    //     }
+            indent--;
 
-    //     public override float GetPropertyHeight(SerializedProperty property, GUIContent label) {
-    //         return EditorGUI.GetPropertyHeight(property);
-    //     }
+            EditorGUI.EndProperty();
+        }
 
-    //     protected virtual void DrawDataField(Rect position, ViewModel<T> viewModel, DataModel<T> dataModel) {
-    //         T newValue = this.DrawField(position, dataModel.Value);
-    //         if (!dataModel.IsEqual(newValue)) {
-    //             dataModel.TryChangeValue(newValue);
-    //         }
-    //     }
-
-    //     protected virtual T DrawField(Rect position, T oldValue) {
-    //         return oldValue;
-    //     }
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label) {
+            return EditorGUI.GetPropertyHeight(property);
+        }
     }
-
 }
